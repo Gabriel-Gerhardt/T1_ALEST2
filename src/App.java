9d1f6e89ac91f6832d76561ea1e8723940c39b33
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-04-09 16:40:48 -0300

    First Commit

--- src/App.java
+++ src/App.java
@@ -1,9 +1,40 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
+        ArrayList<Integer> lista= new ArrayList<>();
+        Utils utils = new Utils();
+        utils.preencherArray(7,lista);
+        ArrayList<Integer> lista2= new ArrayList<>();
 
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        }
+
+
+    public int conta_interacoes(int n, ArrayList<Integer> lista, ArrayList<Integer> lista2) {
+        int cont =0;
+
+        for(int i=0; i<n; i++){
+            Scanner sc = new Scanner(System.in);
+
+        }
+
+        while(!(lista.equals(lista2))){
+        lista
+
+
+        }
+        return cont;
     }
+
+
+
 }
---
+++ src/Utils.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Utils {
+
+
+
+
+    public void preencherArray(int n, ArrayList<Integer> lista){
+        for(int i=0; i<n; i++){
+            lista.add(i);
+        }
+    }
+}
